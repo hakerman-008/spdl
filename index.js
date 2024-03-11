79245dde50a59768e935ba711f9bdad72e61078a
hakerman <142625808+hakerman-008@users.noreply.github.com>
2024-03-11 17:34:27 +0545

    Update index.js

--- index.js
+++ index.js
@@ -3,66 +3,31 @@ const axios = require('axios');
 
 const app = express();
 
-
 app.get('/spotify', async (req, res) => {
     try {
         const spotifyUrl = req.query.id;
         const userQuery = req.query.query;
 
         if (spotifyUrl) {
-          
-            const options = {
-                method: 'GET',
-                url: 'https://spotify-downloader6.p.rapidapi.com/spotify',
-                params: { spotifyUrl },
-                headers: {
-                    'X-RapidAPI-Key': 'b38444b5b7mshc6ce6bcd5c9e446p154fa1jsn7bbcfb025b3b',
-                    'X-RapidAPI-Host': 'spotify-downloader6.p.rapidapi.com'
-                }
-            };
+            const apiUrl = `https://tools.betabotz.eu.org/tools/spotifydl?url=${spotifyUrl}`;
+            const response = await axios.get(apiUrl);
 
-            const response = await axios.request(options);
-            const { download_link } = response.data;
-
-            return res.json({ download_link });
+            if (response.data.status === 200) {
+                const download_link = response.data.result;
+                return res.json({ download_link });
+            } else {
+                return res.status(404).json({ error: 'Download link not found' });
+            }
         } else if (userQuery) {
-           
-            const options = {
-                method: 'GET',
-                url: 'https://spotify81.p.rapidapi.com/search',
-                params: {
-                    q: userQuery,
-                    type: 'tracks',
-                    offset: '0',
-                    limit: '10',
-                    numberOfTopResults: '5'
-                },
-                headers: {
-                    'X-RapidAPI-Key': 'b38444b5b7mshc6ce6bcd5c9e446p154fa1jsn7bbcfb025b3b',
-                    'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-                }
-            };
-
-            const response = await axios.request(options);
-            const { tracks } = response.data;
-
-          
-            const trackIDs = tracks.map(track => {
-                const trackID = track.uri.split(':')[2];
-                return `https://open.spotify.com/track/${trackID}`;
-            });
-
-            return res.json({ trackIDs });
+            return res.status(400).json({ error: 'Searching by query is not supported with this API' });
         } else {
-           
-            return res.status(400).json({ error: 'Spotify URL or query is required' });
+            return res.status(400).json({ error: 'Spotify URL is required' });
         }
     } catch (error) {
         console.error('Error:', error);
         return res.status(500).json({ error: 'Internal server error' });
     }
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
