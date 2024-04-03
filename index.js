6a0ac60abe7d8d6b3a90ef725827c8566a69519d
hakerman <142625808+hakerman-008@users.noreply.github.com>
2024-04-03 18:37:20 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,33 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+const rapidApiKey = 'b38444b5b7mshc6ce6bcd5c9e446p154fa1jsn7bbcfb025b3b';
 
 app.get('/spotify', async (req, res) => {
     try {
         const spotifyUrl = req.query.id;
-        const userQuery = req.query.query;
 
-        if (spotifyUrl) {
-            const apiUrl = `https://tools.betabotz.eu.org/tools/spotifydl?url=${spotifyUrl}`;
-            const response = await axios.get(apiUrl);
-
-            if (response.data.status === 200) {
-                const download_link = response.data.result;
-                return res.json({ download_link });
-            } else {
-                return res.status(404).json({ error: 'Download link not found' });
-            }
-        } else if (userQuery) {
-            return res.status(400).json({ error: 'Searching by query is not supported with this API' });
-        } else {
+        if (!spotifyUrl) {
             return res.status(400).json({ error: 'Spotify URL is required' });
+        }
+
+        const options = {
+            method: 'GET',
+            url: 'https://spotify-downloader6.p.rapidapi.com/spotify',
+            params: { spotifyUrl },
+            headers: {
+                'X-RapidAPI-Key': rapidApiKey,
+                'X-RapidAPI-Host': 'spotify-downloader6.p.rapidapi.com'
+            }
+        };
+
+        const response = await axios.request(options);
+
+        if (response.data && response.data.download_link) {
+            const { download_link } = response.data;
+            return res.json({ download_link });
+        } else {
+            return res.status(404).json({ error: 'Download link not found' });
         }
     } catch (error) {
         console.error('Error:', error);
